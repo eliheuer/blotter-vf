8bc18b0ac7a5b905a5af38a3bebc92b243be52b8
Eli Heuer <eliheuer@gmail.com>
2019-02-16 11:47:30 -0500

    Add new build scripts

---
+++ BUILD.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 sources/BUILD.py --fontbakery --static --fixnonhinting
--- sources/BUILD.py
+++ sources/BUILD.py
@@ -110,12 +110,12 @@ def intro():
     """
     Gives basic script info.
     """
-    printG("#    # #####                    #####    ################")
-    printG("#    # #                        #   #    #   ##         #")
-    printG(" #  #  ####                      #   #  #   # #   #######")
-    printG(" #  #  #     <---------------->  #    ##    # #      #")
-    printG("  ##   #                          #        #  #   ####")
-    printG("  ##   #                          ##########  #####")
+    printG("#    # #####        #####    ################")
+    printG("#    # #            #   #    #   ##         #")
+    printG(" #  #  ####          #   #  #   # #   #######")
+    printG(" #  #  #     <---->  #    ##    # #      #")
+    printG("  ##   #              #        #  #   ####")
+    printG("  ##   #              ##########  #####")
     print("\n**** Starting variable font build script:")
     print("     [+]", time.ctime())
     printG("    [!] Done")
@@ -194,7 +194,6 @@ def run_fontmake_variable():
             "fontmake \
                       -g sources/%s.glyphs \
                       -o variable \
-                      --verbose DEBUG \
                       --output-path fonts/%s-VF.ttf"
             % (source, source),
             shell=True,
@@ -214,7 +213,7 @@ def run_fontmake_static():
             "fontmake \
                       -g sources/%s.glyphs \
                       -o ttf \
-                      --keep-overlaps -i --verbose DEBUG"
+                      --keep-overlaps -i"
             % (source),
             shell=True,
         )
